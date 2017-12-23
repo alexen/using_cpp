2e334b1437b84d5e21262f73826cceede6ed4d43
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2017-12-23 23:26:59 +0300

    main: Использование пользовательских буферов вывода

--- main.cpp
+++ main.cpp
@@ -11,47 +11,38 @@
 #include <unordered_map>
 #include <tuple>
 #include <vector>
+#include <locale>
+#include <streambuf>
 #include <fstream>
 #include <functional>
 #include <iostream>
 #include <stdexcept>
+#include <system_error>
+#include <boost/utility/string_ref.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 #include <stl_container_io.h>
 #include <person.h>
 #include <person_io.h>
-
-
-class Redirector {
-public:
-     explicit Redirector( std::ostream& from )
-          : ostr_( from )
-     {}
-
-     void to( std::ostream& to )
-     {
-          streambuf_ = ostr_.rdbuf( to.rdbuf() );
-     }
-
-     ~Redirector()
-     {
-          if( streambuf_ )
-          {
-               ostr_.rdbuf( streambuf_ );
-          }
-     }
-
-private:
-     std::ostream& ostr_;
-     std::streambuf* streambuf_ = nullptr;
-};
+#include <user_streambuf.h>
 
 
 int main()
 {
      try
      {
-          std::cout << std::make_tuple( "Pi is", ':', 3.14 ) << "\n";
-          std::cout << std::array< float, 4 >{ 1.2, 3.4, 2.1, 0.5 } << "\n";
+          FILE* fptr = fopen( "/tmp/output.txt", "w" );
+          if( !fptr )
+          {
+               BOOST_THROW_EXCEPTION( std::system_error( errno, std::system_category() ) );
+          }
+          std::unique_ptr< FILE, decltype( &fclose ) > file{ fptr, fclose };
+          FdStreambuf strbuf{ fileno( file.get() ) };
+          std::ostream ostr{ &strbuf };
+          ostr << '@' << "\n"
+               << std::make_tuple( 3.14, "hello", '1') << "\n"
+               << 1 << '\n'
+               << 2  << '\n'
+               << 2.18 << '\n';
      }
      catch( const std::exception& e )
      {
