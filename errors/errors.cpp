8f4edc9e64b2972e9b19a9323472ed49dcba2d61
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2020-11-07 20:06:10 +0300

    Добавлена директория для работы с типами ошибок

---
+++ errors/errors.cpp
@@ -0,0 +1,12 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#include <errors/errors.h>
+
+
+namespace using_cpp {
+namespace errors {
+
+} // namespace errors
+} // namespace using_cpp
---
+++ errors/errors.h
@@ -0,0 +1,12 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#pragma once
+
+
+namespace using_cpp {
+namespace errors {
+
+} // namespace errors
+} // namespace using_cpp
